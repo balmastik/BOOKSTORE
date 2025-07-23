f7fc817c636381a06fc3699bfef7f288f2410aae
Olesja <bondarenkooa@seznam.cz>
2025-07-23 20:14:23 +0200

    dependencies update

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = merge(common, {
